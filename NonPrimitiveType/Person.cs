c7fc476b26e8b1015fd4606c778d142ebda4709b
Kindeme <fkounoudji@gmail.com>
2020-10-23 23:10:47 -0500

    Add project files.

---
+++ NonPrimitiveType/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NonPrimitiveType
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void Introduce()
+        {
+            Console.WriteLine("My name is {0} {1}",FirstName , LastName);
+        }
+    }
+}
---
+++ NonPrimitiveType/Program.cs
@@ -0,0 +1,26 @@
+﻿using NonPrimitiveType.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonPrimitiveType
+{
+    class Program
+    {
+        private static Person john;
+
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
