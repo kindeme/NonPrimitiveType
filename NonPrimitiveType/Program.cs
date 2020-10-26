8d82133ed5cfb7b48b05dec810aaf1d453fbaf94
Kindeme <fkounoudji@gmail.com>
2020-10-25 22:45:19 -0500

    string type

--- NonPrimitiveType/Program.cs
+++ NonPrimitiveType/Program.cs
@@ -21,6 +21,9 @@ namespace NonPrimitiveType
             // var result = calculator.Add(1, 2);
             //  Console.WriteLine(result);
             var numbers = new int[3];
+
+            // Arrays
+
             numbers[0] = 1;
             Console.WriteLine(numbers[0]);
              Console.WriteLine(numbers[1]);
@@ -32,12 +35,24 @@ namespace NonPrimitiveType
             flags[0] = true;
             Console.WriteLine(flags[0]);
             Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[02]);
+            Console.WriteLine(flags[2]);
 
             // objeect initialisation syntax
             var names = new string[3] { "Jack", "John", "Mary" };
 
+            // string
 
+
+            var names2 = new string[3] { "John", "Jack", "Mary" };
+            var formatttedNames = string.Join(",", names2);
+            Console.WriteLine(formatttedNames);
+
+            //string firstName = "Kindeme";
+            //string lastName = "Kounoudji";
+            // string myFullName = string.Format("My name is {{0)} {{1}}",firstName,lastName);
+            // Console.WriteLine(myFullName);
+
+            
 
         }
     }
